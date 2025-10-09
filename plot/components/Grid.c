bd4c210eac756339d580e154df7583a6f723e8fd
szymusu <szymusu@gmail.com>
2025-10-09 16:45:59 +0200

    grid fixed

--- plot/components/Grid.c
+++ plot/components/Grid.c
@@ -19,6 +19,8 @@ void Grid(const double step, const Bounds bounds) {
 
     Vector2 start = {.y = PLOT_OFFSET_Y};
     Vector2 end = {.y = PLOT_HEIGHT + PLOT_OFFSET_Y};
+    const int zero_x = transform_x_to_pixel(0, bounds.start_x, bounds.end_x);
+    const int zero_y = transform_y_to_pixel(0, bounds.start_y, bounds.end_y);
     char number[16];
     for (double x = 0.; x <= bounds.end_x; x += step) {
         const float px = transform_x_to_pixelf(x, bounds.start_x, bounds.end_x);
@@ -26,7 +28,7 @@ void Grid(const double step, const Bounds bounds) {
         end.x = px;
         DrawLineEx(start, end, 1.f, GRAY);
         sprintf(number, "%.2f", x);
-        DrawText(number, (int) px, transform_y_to_pixel(0, bounds.start_y, bounds.end_y), 10, BLACK);
+        DrawText(number, (int) px, zero_y, 10, BLACK);
     }
     for (double x = -step; x >= bounds.start_x; x -= step) {
         const float px = transform_x_to_pixelf(x, bounds.start_x, bounds.end_x);
@@ -34,24 +36,26 @@ void Grid(const double step, const Bounds bounds) {
         end.x = px;
         DrawLineEx(start, end, 1.f, GRAY);
         sprintf(number, "%.2f", x);
-        DrawText(number, (int) px, transform_y_to_pixel(0, bounds.start_y, bounds.end_y), 10, BLACK);
+        DrawText(number, (int) px, zero_y, 10, BLACK);
     }
     start.x = PLOT_OFFSET_X;
     end.x = PLOT_WIDTH + PLOT_OFFSET_X;
-    for (double y = step; y <= bounds.end_y; y += step) {
+    for (double y = step;; y += step) {
         const float py = transform_y_to_pixelf(y, bounds.start_y, bounds.end_y);
+        if (py < PLOT_OFFSET_Y) break;
         start.y = py;
         end.y = py;
         DrawLineEx(start, end, 1.f, GRAY);
         sprintf(number, "%.2f", y);
-        DrawText(number, transform_x_to_pixel(0, bounds.start_x, bounds.end_x), (int) py, 10, BLACK);
+        DrawText(number, zero_x, (int) py, 10, BLACK);
     }
-    for (double y = -step; y >= bounds.start_y; y -= step) {
+    for (double y = -step;; y -= step) {
         const float py = transform_y_to_pixelf(y, bounds.start_y, bounds.end_y);
+        if (py > PLOT_OFFSET_Y + PLOT_HEIGHT) break;
         start.y = py;
         end.y = py;
         DrawLineEx(start, end, 1.f, GRAY);
         sprintf(number, "%.2f", y);
-        DrawText(number, transform_x_to_pixel(0, bounds.start_x, bounds.end_x), (int) py, 10, BLACK);
+        DrawText(number, zero_x, (int) py, 10, BLACK);
     }
 }
