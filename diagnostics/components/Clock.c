c921f8593cda8a08bb43d955c4d82ee704d2c9d7
szymusu <szymusu@gmail.com>
2025-10-13 11:02:20 +0200

    dont use x86 intrinsics if on arm

--- diagnostics/components/Clock.c
+++ diagnostics/components/Clock.c
@@ -2,7 +2,8 @@
 
 #ifdef _WIN32
 #include <intrin.h>
-#else
+#endif
+#ifndef __arm64
 #include <x86intrin.h>
 #endif
 
@@ -17,12 +18,16 @@ clock_t start = 0, end = 0;
 unsigned long long start_cycles = 0, end_cycles = 0;
 
 void clock_start() {
+#ifndef __arm64
     start_cycles = __rdtsc();
+#endif
     start = clock();
 }
 
 void clock_end() {
+#ifndef __arm64
     end_cycles = __rdtsc();
+#endif
     end = clock();
     sprintf(clock_text, "Cycles: %ld\nms: %lf", end_cycles - start_cycles, (end - start) * 1000. / CLOCKS_PER_SEC);
 }
