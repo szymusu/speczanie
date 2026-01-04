e0bf5e88ec1dfa03ec3b312e1b2b97712ad9ee0e
szymusu <szymusu@gmail.com>
2026-01-04 19:42:28 +0100

    arrow color

--- plot/components/Arrow.c
+++ plot/components/Arrow.c
@@ -6,16 +6,18 @@
 
 #define ARROW_PADDING 0.3f
 #define ARROW_HEAD_ANGLE 0.6f
+#define ARROW_COLOR (Color){200, 70, 85, 150}
 
 void Arrow(Vector2 origin, const Vector2 direction, const Bounds bounds) {
     const float magnitude = vec2_magnitude(direction);
     Vector2 end = { origin.x + direction.x, origin.y - direction.y };
 
     if (magnitude < ARROW_PADDING * 2) {
-        DrawLineV(
+        DrawLineEx(
         transform_v_to_pixel(origin, bounds),
         transform_v_to_pixel(end, bounds),
-        RED);
+        2,
+        ARROW_COLOR);
         return;
     }
     const float inv_magnitude = 1 / magnitude;
@@ -34,16 +36,19 @@ void Arrow(Vector2 origin, const Vector2 direction, const Bounds bounds) {
     head_left.x -= cosf(angle + ARROW_HEAD_ANGLE) * ARROW_PADDING;
     head_left.y += sinf(angle + ARROW_HEAD_ANGLE) * ARROW_PADDING;
 
-    DrawLineV(
+    DrawLineEx(
         transform_v_to_pixel(origin, bounds),
         transform_v_to_pixel(end, bounds),
-        RED);
-    DrawLineV(
+        2,
+        ARROW_COLOR);
+    DrawLineEx(
         transform_v_to_pixel(end, bounds),
         transform_v_to_pixel(head_right, bounds),
-        RED);
-    DrawLineV(
+        2,
+        ARROW_COLOR);
+    DrawLineEx(
         transform_v_to_pixel(end, bounds),
         transform_v_to_pixel(head_left, bounds),
-        RED);
+        2,
+        ARROW_COLOR);
 }
