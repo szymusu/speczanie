2d3502b47fd4c4fd8cfe865602573cb298460cd4
szymusu <szymusu@gmail.com>
2026-01-17 19:41:53 +0100

    better fps avg, max frame time

--- diagnostics/components/FpsCounter.c
+++ diagnostics/components/FpsCounter.c
@@ -6,25 +6,29 @@
 
 #include "../../text/text.h"
 
+#define COUNT 4096
+
 void FpsCounter() {
     static char text[64] = {0};
-    static int min = 999999;
-    static int buffer[256] = {0};
+    static int buffer[COUNT] = {0};
     static int sum = 0;
-    static uint8_t index = 0;
+    static int index = 0;
+    static float max_time = 0;
 
     const int current = GetFPS();
-    if (current < min) min = current;
 
     const int old = buffer[index];
     sum = sum - old + current;
     buffer[index] = current;
 
-    ++index;
+    if (++index == COUNT) index = 0;
+
+    const float time = GetFrameTime();
+    if (time > max_time) max_time = time;
 
     DrawRectangle(0, 0, 65, (15-3)*3+10, RAYWHITE);
 
-    sprintf(text, "FPS %d\nmin %d\navg %d", current, min, sum / 256);
+    sprintf(text, "FPS %d\navg %d\nmax %.4f ms", current, sum / COUNT, max_time);
     SetTextLineSpacing(-3);
     Text(text, 5, 5, 15, BLACK);
     SetTextLineSpacing(0);
