eb9611c04704480930c725abbdd7e087d30767e2
szymusu <szymusu@gmail.com>
2026-01-17 13:39:39 +0100

    fix null file loaded on drop

--- main.c
+++ main.c
@@ -68,13 +68,16 @@ int main(const int argc, char** argv) {
         const int clicked = FileList();
         if (clicked != -1) {
             float zoom = 1;
+            float scale_x = 1;
             Vector2 pan = {0,0};
             if (current_file) {
                 zoom = current_file->data_plot_state.zoom;
+                scale_x = current_file->data_plot_state.scale_x;
                 pan = current_file->data_plot_state.pan;
             }
             current_file = select_file(clicked);
             current_file->data_plot_state.zoom = zoom;
+            current_file->data_plot_state.scale_x = scale_x;
             current_file->data_plot_state.pan = pan;
             change |= MOVE_CHANGE_PLOT;
         }
@@ -90,7 +93,7 @@ int main(const int argc, char** argv) {
                 last = open_file(files.paths[i]);
             }
             if (last != -1){
-                current_file = select_file(clicked);
+                current_file = select_file(last);
                 change |= MOVE_CHANGE_PLOT;
             }
             UnloadDroppedFiles(files);
